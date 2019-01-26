941e3ff46a0d6a2c81e828181a8245699cd2a2e0
Vftdan <vftdan@gmail.com>
2019-01-27 00:56:03 +0300

    Inline mode

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 from telebot import TeleBot, apihelper
+from telebot.types import InlineQueryResultArticle as IQRA
+from telebot.types import InputTextMessageContent as ITMC
 import json
 from texfmt import tex2html, TexException
 
@@ -6,13 +8,46 @@ conf = json.load(open('priv.json'))
 bot = TeleBot(conf['key'])
 apihelper.proxy = conf.get('proxy', apihelper.proxy)
 
+def Constant(c):
+    def f(*args, **kwargs):
+        return c
+    return f
+
 def stripcmd(txt):
     if txt == '' or txt[0] != '/':
         return txt
     return ' '.join(txt.split(' ')[1:])
 
+def nohtml(html):
+    ents = {'lt': '<', 'gt': '>', 'amp': '&', 'quot': '"'}
+    mode_ent = False
+    mode_tag = False
+    res = []
+    ent = ''
+    for c in html:
+        if mode_tag:
+            if c == '>':
+                mode_tag = False
+            continue
+        if mode_ent:
+            if c == ';':
+                mode_ent = False
+                res.append(ents.get(ent, '&{0};'.format(ent)))
+                ent = ''
+                continue
+            ent += c
+            continue
+        if c == '&':
+            mode_ent = True
+            continue
+        if c == '<':
+            mode_tag = True
+            continue
+        res.append(c)
+    return ''.join(res)
+
 @bot.message_handler(commands=['tex'])
-def texfmt(msg):
+def texcmd(msg):
     try:
         try:
             bot.reply_to(msg, tex2html(stripcmd(msg.text), True), parse_mode='HTML')
@@ -23,5 +58,17 @@ def texfmt(msg):
             print(e)
     except BaseException as e:
         print(e)
+
+@bot.inline_handler(Constant(True))
+def inlinequeryhandler(iq):
+    try:
+        if len(iq.query) == 0:
+            return
+        result = tex2html(stripcmd(iq.query), True)
+        bot.answer_inline_query(iq.id, [
+            IQRA('1', 'TeX format', ITMC(result, parse_mode='HTML'), description=nohtml(result))
+            ])
+    except BaseException as e:
+        pass
 
 bot.polling()
